2f8182e523a61e4491de2c3f4b175af32a5b3898
Mat <shermanmat1@gmail.com>
2017-02-13 18:57:05 +0000

    Initial deploy commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose")
 var app = express();
+var passport =  require("passport")
+var LocalStrategy = require("passport-local")
+var User = require("./models/users")
 
 
 mongoose.connect("mongodb://localhost/help_a_newbie");
@@ -26,9 +29,26 @@ var Community = mongoose.model("Community", communitySchema);
     
     
     
+app.use(require("express-session")({
+    
+    
+    secret: "#yesphx",
+    resave: false,
+    saveUninitialized: false
+    
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy( User.authenticate()))
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-
+app.use(function(req, res, next){
+    
+    res.locals.currentUser = req.user;
+    next();
+});
 
 // ROutes
 
@@ -39,6 +59,8 @@ app.get("/", function(req, res){
 
 app.get("/community", function(req, res){
     
+
+    
     Community.find({}, function(err, allCommunity){
         
         if (err){
@@ -46,7 +68,7 @@ app.get("/community", function(req, res){
             console.log("error for now")
         } else {
             
-            res.render("index", {community: allCommunity});
+            res.render("index", {community: allCommunity, currentUser: req.user});
         }
         
     })
@@ -56,7 +78,7 @@ app.get("/community", function(req, res){
 
 });
 
-app.get("/community/new", function(req, res){
+app.get("/community/new", isLoggedIn,  function(req, res){
     
     
     res.render("new.ejs")
@@ -85,8 +107,75 @@ Community.create(newCommunity, function( err, newlyCreated) {
 
 app.get("/community/:id", function (req, res){
     
-    res.render ("show");
+    res.render ("/login");
+});
+
+
+
+// AUTH Routes
+
+
+app.get("/register", function(req, res){
+    res.render("register");
+    
+});
+
+app.post("/register", function(req, res){
+    
+
+       var newUser = new User({username: req.body.username});
+       User.register(newUser, req.body.password, function(err, user){
+           
+           if (err){
+               
+               console.log(err)
+               return res.render("register")
+           }
+           
+           passport.authenticate("local")(req, res, function(){
+               
+               res.redirect("/community")
+               
+           })
+       })
+       
+});
+
+
+app.get("/login", function(req, res){
+    
+    res.render("login");
+});
+
+
+app.post("/login", passport.authenticate("local", 
+
+{
+    successRedirect: "/community", 
+    failureRedirect: "/login"
+    
+    
+    
+}), function(req, res){
+    
+    passport.authenticate()
 })
+
+
+app.get("/logout", function(req, res){
+    
+    req.logout();
+    res.redirect("/community")
+})
+
+function isLoggedIn(req, res, next){
+    
+    if(req.isAuthenticated()){
+        
+        return next();
+        
+    } res.redirect("/login")
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The server is up lets gooooo");
