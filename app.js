232ea850a21c82db56f250928700d17782ac630b
Mat <shermanmat1@gmail.com>
2017-02-17 04:33:47 +0000

    Second push to finish off v 1

--- app.js
+++ app.js
@@ -55,9 +55,39 @@ app.use(function(req, res, next){
 // ROutes
 
 app.get("/", function(req, res){
+    
+    
+    
      
     res.render("welcome")
 });
+
+app.get("/nextsteps", function(req, res) {
+    
+    
+    var helper = require('sendgrid').mail;
+  
+var from_email = new helper.Email("shermanmat1@gmail.com");
+var to_email = new helper.Email("mat.sherman@helloendless.com");
+var subject = "Sending with SendGrid is Fun";
+var content = new helper.Content("text/plain", "and easy to do anywhere, even with Node.js");
+var mail = new helper.Mail(from_email, subject, to_email, content);
+
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON()
+});
+
+sg.API(request, function(error, response) {
+  console.log(response.statusCode);
+  console.log(response.body);
+  console.log(response.headers);
+})
+    
+    res.render("nextsteps")
+})
 
 app.get("/community", function(req, res){
     
@@ -149,6 +179,12 @@ app.post("/register", function(req, res){
            })
        })
        
+});
+
+app.get("/faq", function(req, res){
+    
+    res.render("faq")
+    
 });
 
 
